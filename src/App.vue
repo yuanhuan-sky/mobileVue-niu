9aab1acb2aa3b9401c7d301a0c15b389a9af159f
牛亮 <nll@itcast.cn>
2019-08-08 11:04:35 +0800

    登录组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <template>
   <div id="app">
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-
-    <div class="box"></div>
-
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-.box {
-  width: 750px;
-  height: 60px;
-  background-color: red;
-}
 </style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-wrap">
+    <van-nav-bar
+      title="标题"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
