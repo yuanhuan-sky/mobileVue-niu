a353c1875e39314e4d85d4d2ef72f5580125975f
牛亮 <nll@itcast.cn>
2019-08-08 10:51:27 +0800

    配置rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,34 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+// sep 路径中的分隔符，为了适配不同的操作系统
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  // file 编译的时候，处理的样式文件 file
+  //     css/less/sass/vue
+  // 判断文件路径中是否包含 vant，是否是vant提供的样式，如果是设置rootValue=37.5
+  // 如果是自己写的样式，设置rootValue=750
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
+
+// module.exports = {
+//   plugins: {
+//     'autoprefixer': {
+//       overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7']
+//     },
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="box"></div>
+
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+.box {
+  width: 750px;
+  height: 60px;
+  background-color: red;
+}
 </style>
