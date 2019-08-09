947a53babb1805fa0f1db38b55a59995c8eaeda3
牛亮 <nll@itcast.cn>
2019-08-09 10:01:23 +0800

    list组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,15 +7,53 @@
     <!-- 频道列表 -->
     <van-tabs>
       <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
-        内容 {{ index }}
+        <!-- 文章列表-自带上拉加载更多 -->
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+          <van-cell
+            v-for="item in list"
+            :key="item"
+            :title="item"
+          />
+        </van-list>
       </van-tab>
     </van-tabs>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      // list 需要的数据
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 1000);
+    }
+  }
 }
 </script>
 
