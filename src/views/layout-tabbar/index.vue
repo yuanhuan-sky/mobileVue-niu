1718d31a194ce2c956cf018c769a70b4c46edda2
牛亮 <nll@itcast.cn>
2019-08-09 09:28:36 +0800

    布局组件和home组件

---
+++ src/views/layout-tabbar/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-view></router-view>
+
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/">主页</van-tabbar-item>
+      <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
+
