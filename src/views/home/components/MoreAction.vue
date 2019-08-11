754283a975b78e53e7f10f8277adebabcb6b8319
牛亮 <nll@itcast.cn>
2019-08-11 12:21:09 +0800

    反馈垃圾内容-优化

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -20,9 +20,14 @@
       <van-cell icon="arrow-left" @click="isReportShow=false"/>
   <!-- // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权' -->
 
-      <van-cell title="标题夸张" icon="location-o" @click="handle('report', 1)" />
-      <van-cell title="低俗色情" icon="location-o" @click="handle('report', 2)" />
-      <van-cell title="错别字多" icon="location-o" @click="handle('report', 3)" />
+      <van-cell
+        v-for="item in list"
+        :title="item.title"
+        :key="item.type"
+        icon="location-o"
+        @click="handle('report', item.type)" />
+      <!-- <van-cell title="低俗色情" icon="location-o" @click="handle('report', 2)" />
+      <van-cell title="错别字多" icon="location-o" @click="handle('report', 3)" /> -->
     </van-cell-group>
   </van-dialog>
 </template>
@@ -37,7 +42,19 @@ export default {
   data () {
     return {
       // 控制反馈内容是否显示
-      isReportShow: false
+      isReportShow: false,
+      // 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权
+      list: [
+        { title: '标题夸张', type: 1 },
+        { title: '低俗色情', type: 2 },
+        { title: '错别字多', type: 3 },
+        { title: '旧闻重复', type: 4 },
+        { title: '广告软文', type: 5 },
+        { title: '内容不实', type: 6 },
+        { title: '涉嫌违法犯罪', type: 7 },
+        { title: '侵权', type: 8 },
+        { title: '其他问题', type: 0 },
+      ]
     }
   },
   methods: {
