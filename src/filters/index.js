577902c835ac2c0e03435ad611e21b59d5e301b0
牛亮 <nll@itcast.cn>
2019-08-11 09:27:25 +0800

    dayjs处理相对时间-过滤器

---
+++ src/filters/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import dayjs from 'dayjs'
+
+import relativeTime from 'dayjs/plugin/relativeTime'
+// 安装dayjs相对时间处理的插件
+dayjs.extend(relativeTime)
+
+Vue.filter('fmtDate', (value) => {
+  console.log('dayjs', dayjs())
+  console.log('dayjs(value)', dayjs(value))
+  console.log('dayjs().to(dayjs(value))', dayjs().to(dayjs(value)))
+  console.log('dayjs().from(dayjs(value))', dayjs().from(dayjs(value)))
+
+  return dayjs().to(dayjs(value))
+})
