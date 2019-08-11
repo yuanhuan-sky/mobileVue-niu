1ad5f893a92f39b94d03c5e82b2d414711c69cc4
牛亮 <nll@itcast.cn>
2019-08-11 10:54:38 +0800

    MoreAction中显示的cell

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -10,14 +10,31 @@
     @input="$emit('input', $event)"
     closeOnClickOverlay
   >
-    Hello world
+    <van-cell-group v-show="!isReportShow">
+      <van-cell title="不感兴趣" icon="location-o" />
+      <van-cell title="反馈垃圾内容" icon="location-o" is-link @click="isReportShow=true" />
+      <van-cell title="拉黑作者" icon="location-o" />
+    </van-cell-group>
+
+    <van-cell-group v-show="isReportShow">
+      <van-cell icon="arrow-left" @click="isReportShow=false"/>
+      <van-cell title="标题夸张" icon="location-o" />
+      <van-cell title="低俗色情" icon="location-o" />
+      <van-cell title="错别字多" icon="location-o" />
+    </van-cell-group>
   </van-dialog>
 </template>
 
 <script>
 export default {
   name: "MoreAction",
-  props: ['value']
+  props: ['value'],
+  data () {
+    return {
+      // 控制反馈内容是否显示
+      isReportShow: false
+    }
+  }
 }
 </script>
 
