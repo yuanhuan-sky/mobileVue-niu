8a1319b70f63469e525cafdbbd5534e83850c5e3
牛亮 <nll@itcast.cn>
2019-08-13 11:23:57 +0800

    搜索功能-搭建搜索界面

---
+++ src/views/search/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 导航头-搜索框 -->
+    <van-search
+      placeholder="请输入搜索关键词"
+      v-model="value"
+      show-action
+      @search="onSearch"
+      @cancel="$router.push('/')"
+    />
+
+    <!-- 智能提示 -->
+    <van-cell-group>
+      <van-cell title="单元格" icon="search"/>
+      <van-cell title="单元格" icon="search"/>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch () {
+      console.log('xxx')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
