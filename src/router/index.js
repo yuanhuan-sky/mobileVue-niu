665115009e6c10a99b2308069f10fb156e9973f7
牛亮 <nll@itcast.cn>
2019-08-14 09:55:29 +0800

    搜索功能-搜索结果页面和路由传参

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,12 @@ export default new Router({
       component: () => import(/* webpackChunkName: "search" */ '@/views/search/index.vue')
     },
     {
+      name: 'search-result',
+      path: '/search-result/:q',
+      props: true,
+      component: () => import(/* webpackChunkName: "search-result" */ '@/views/search-result/index.vue')
+    },
+    {
       path: '/',
       component: () => import(/* webpackChunkName: "layout" */ '@/views/layout-tabbar/index.vue'),
       children: [
---
+++ src/views/search-result/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    搜索结果
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['q'],
+  created () {
+    console.log(this.q)
+  }
+// 获取动态路由参数
+// this.$route.params.id
+}
+</script>
+
+<style>
+
+</style>
