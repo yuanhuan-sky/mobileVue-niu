298485f2276d6be65d4994a920a91b8853b46a7a
牛亮 <nll@itcast.cn>
2019-08-14 12:42:08 +0800

    refresh_token的实现

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
 import JSONbig from 'json-bigint'
 
 const request = axios.create({
@@ -39,7 +40,43 @@ request.interceptors.response.use(function (response) {
   // 更方便的让外面获取到响应中的数据
   // Do something with response data
   return response.data.data || response.data
-}, function (error) {
+}, async function (error) {
+  // console.log(error)
+  // console.dir(error)
+  // 判断是否是401 如果是401（没有登录或者登录过期）
+  if (error.response.status === 401) {
+    // 判断是否登录，如果没有登录，跳转到登录页面
+    if (!store.state.user) {
+      router.push({
+        name: 'login'
+      })
+      return
+    }
+    try {
+      // 如果是登录过期，刷新token
+      const refreshToken = store.state.user.refresh_token
+      const res = await axios({
+        method: 'put',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          'Authorization': `Bearer ${refreshToken}`
+        }
+      })
+
+      // 重置 vuex中user的值
+      store.commit('setUser', {
+        token: res.data.data.token,
+        refresh_token: refreshToken
+      })
+      // 继续上一次出问题的请求
+      return request(error.config)
+    } catch (err) {
+      router.push({
+        name: 'login'
+      })
+    }
+  }
+
   // Do something with response error
   return Promise.reject(error)
 })
