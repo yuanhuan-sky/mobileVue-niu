78578061d95741f01332acec2882722ed4e6b5bb
牛亮 <nll@itcast.cn>
2019-08-17 10:50:07 +0800

    选择图片预览功能

--- src/views/user-profile/components/UploadPhoto.vue
+++ src/views/user-profile/components/UploadPhoto.vue
@@ -6,7 +6,8 @@
     closeOnClickOverlay
   >
     <van-cell-group>
-      <van-cell title="从相册选择" />
+      <van-cell title="从相册选择" @click="handleSelectPhoto" />
+      <input type="file" @change="handleFile" ref="file" style="display: none">
       <van-cell title="拍照" />
       <van-cell title="取消" @click="$emit('input', false)" />
     </van-cell-group>
@@ -14,12 +15,39 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { ImagePreview } from 'vant'
+
+Vue.use(ImagePreview)
 export default {
   name: 'UploadPhoto',
   props: ['value'],
   data () {
     return {
       show: true
+    }
+  },
+  methods: {
+    // 点击从相册选择，弹出选择文件的对话框
+    handleSelectPhoto () {
+      // 获取file标签，点击
+      this.$refs.file.click()
+    },
+    // 当从file中选择文件后要去预览图片
+    handleFile () {
+      //this.$refs.file.files[0]
+      // 关闭选择图片的弹出框
+      this.$emit('input', false)
+      // 图片的预览
+      ImagePreview({
+        images: [
+          URL.createObjectURL(this.$refs.file.files[0])
+        ],
+        showIndex: false,
+        onClose() {
+          // do something
+        }
+      })
     }
   }
 }
