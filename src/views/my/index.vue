208411806cf7ff08a89e8f284d8dafd463492a70
牛亮 <nll@itcast.cn>
2019-08-17 09:18:22 +0800

    用户信息界面

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <!-- 未登录 -->
-    <un-login></un-login>
+    <un-login v-if="!user"></un-login>
+    <!-- 已登录 -->
+    <user-info v-else></user-info>
   </div>
 </template>
 
 <script>
 import UnLogin from './components/UnLogin'
+import UserInfo from './components/UserInfo'
+import { mapState } from 'vuex'
 export default {
   name: 'My',
   components: {
-    UnLogin
+    UnLogin,
+    UserInfo
+  },
+  computed: {
+    ...mapState(['user'])
   }
 }
 </script>
